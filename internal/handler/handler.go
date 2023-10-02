108be4be60c0093caf9904ab420d76d6c5b38df7
Denis Sirotkin <sirotkin@denis-02.ru>
2023-10-02 21:40:53 +0300

    [/]fix logic for post request

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,14 +4,24 @@ import (
 	"Service/internal/database"
 	"Service/internal/shortlink"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
+
+type RequestBody struct {
+	Url string `json:"url"`
+}
 
 func HandleLink(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		shortLink := r.FormValue("shortlink")
+		if shortLink == "" {
+			http.Error(w, "Ошибка, ссылка не может быть пустой строкой", http.StatusBadRequest)
+			return
+		}
 
 		originalLink, err := database.GetOriginalLink(shortLink)
 		if err != nil {
@@ -29,12 +39,30 @@ func HandleLink(w http.ResponseWriter, r *http.Request) {
 		// http.Redirect(w, r, originalLink, http.StatusFound)
 
 	case http.MethodPost:
-		originalLink := r.FormValue("url")
+		body, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			http.Error(w, "Ошибка чтения тела запроса", http.StatusInternalServerError)
+			return
+		}
+		// Распаковка данных в структуру RequestBody
+		var requestBody RequestBody
+		if err := json.Unmarshal(body, &requestBody); err != nil {
+			http.Error(w, "Ошибка разбора JSON", http.StatusBadRequest)
+			return
+		}
+
+		// Извлечение параметра "url" из тела запроса
+		originalLink := requestBody.Url
+		if originalLink == "" {
+			http.Error(w, "Ошибка, ссылка не может быть пустой строкой", http.StatusBadRequest)
+			return
+		}
 
 		shortLink, err := shortlink.GenerateShortLink(originalLink)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprintf(w, "Ошибка генерации сокращенной ссылки: %s", err)
+			http.Error(w, fmt.Sprintf("Ошибка генерации сокращенной ссылки: %s", err), http.StatusBadRequest)
 			return
 		}
 
